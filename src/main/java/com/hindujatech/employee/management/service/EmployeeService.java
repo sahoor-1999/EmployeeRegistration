39264abd4f648b8ca50ca0a5188f1e9654db4e1c
sahoor-1999 <157471652+sahoor-1999@users.noreply.github.com>
2024-04-12 18:50:42 +0530

    Add files via upload

--- src/main/java/com/hindujatech/employee/management/service/EmployeeService.java
+++ src/main/java/com/hindujatech/employee/management/service/EmployeeService.java
@@ -5,9 +5,10 @@ import com.hindujatech.employee.management.entity.Employee;
 import java.util.List;
 
 public interface EmployeeService {
-    Employee saveEmployee(Employee employee);
+    void saveEmployee(Employee employee, String message);
     void deleteEmployee(Long id);
     List<Employee> getAllEmployee();
     Employee getEmployeeById(Long id);
     Employee updateEmployee(Employee employee);
+    public void sendSMS(String to, String message);
 }
