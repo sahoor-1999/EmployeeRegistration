6c268da6b6fa2a31519a57f54c9ad0e42ef9d21b
sahoor-1999 <157471652+sahoor-1999@users.noreply.github.com>
2024-04-12 18:51:14 +0530

    Add files via upload

--- src/main/java/com/hindujatech/employee/management/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/hindujatech/employee/management/service/impl/EmployeeServiceImpl.java
@@ -5,13 +5,28 @@ import com.hindujatech.employee.management.exception.ResourceNotFoundException;
 import com.hindujatech.employee.management.repository.EmployeeRepository;
 import com.hindujatech.employee.management.service.EmployeeService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private ModelMapper modelMapper;
     private EmployeeRepository employeeRepository;
+
+    @Value("${twilio.accountSid}")
+    private String accountSID;
+    @Value("${twilio.authToken}")
+    private String authToken;
+    @Value("${twilio.phoneNumber}")
+    private String phoneNumber;
 
     public EmployeeServiceImpl(ModelMapper modelMapper, EmployeeRepository employeeRepository) {
         this.modelMapper = modelMapper;
@@ -19,10 +34,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public Employee saveEmployee(Employee employee) {
-        Employee save = employeeRepository.save(employee);
-        return save;
-    }
+    public void saveEmployee(Employee employee, String message) {
+        String mobile = employee.getMobile();
+        boolean b = validateNumber(mobile);
+        if(b == true) {
+            Employee save = employeeRepository.save(employee);
+            if (save != null){
+                sendSMS("+91-"+save.getMobile(), message);
+                }
+            }
+        else
+            throw new ResourceNotFoundException("Mobile Number is not valid.");
+        }
 
     @Override
     public void deleteEmployee(Long id) {
@@ -51,5 +74,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     public Employee updateEmployee(Employee employee) {
         Employee save = employeeRepository.save(employee);
         return save;
+    }
+
+    @Override
+    public void sendSMS(String to, String message) {
+    Twilio.init(accountSID, authToken);
+    Message.creator(new PhoneNumber(to), new PhoneNumber(phoneNumber), message).create();
+    }
+
+    public static boolean validateNumber(String mobileNumber){
+        String regex = "^[0-9]*";
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(mobileNumber);
+        return matcher.matches();
     }
 }
